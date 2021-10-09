26d553adcec4b9ade58b215bbc7bf0f26e94514c
uga-rosa <uga6603@gmail.com>
2021-10-09 23:00:32 +0900

    Only json2lua.lua is back

---
+++ misc/json2lua.lua
@@ -0,0 +1,9 @@
+-- local name = "dict.json"
+local name = "verb.json"
+
+print("return {")
+local json = require("json").decode(io.open(name):read("*a"))
+for k, v in pairs(json) do
+  print('["' .. k .. '"] = "' .. v:gsub('"', '\\"') .. '",')
+end
+print("}")
