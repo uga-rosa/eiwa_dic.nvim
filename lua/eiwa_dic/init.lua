96abec6ef76fffa4598db95b3da4ec5daf6cce57
uga-rosa <uga6603@gmail.com>
2021-10-28 05:29:16 +0900

    indent 4

--- lua/eiwa_dic/init.lua
+++ lua/eiwa_dic/init.lua
@@ -4,90 +4,90 @@ local a = vim.api
 local f = vim.fn
 
 local function code_decision(num)
-  if num < 127 then
-    return true
-  elseif num < 161 then
+    if num < 127 then
+        return true
+    elseif num < 161 then
+        return false
+    elseif num < 224 then
+        return true
+    end
     return false
-  elseif num < 224 then
-    return true
-  end
-  return false
 end
 
 local function head(str)
-  if str == "" then
-    return "", nil, 0
-  elseif #str == 1 then
-    return str, nil, 1
-  elseif code_decision(str:byte()) then
-    return str:sub(1, 1), str:sub(2), 1
-  end
-  return str:sub(1, 3), str:sub(4), 2
+    if str == "" then
+        return "", nil, 0
+    elseif #str == 1 then
+        return str, nil, 1
+    elseif code_decision(str:byte()) then
+        return str:sub(1, 1), str:sub(2), 1
+    end
+    return str:sub(1, 3), str:sub(4), 2
 end
 
 local function cut(str, num)
-  local res = { {} }
-  local init, len = "", 0
-  local c = 1
-  repeat
-    local width
-    init, str, width = head(str)
-    len = len + width
-    if len > num then
-      c = c + 1
-      len = width
-      res[c] = {}
+    local res = { {} }
+    local init, len = "", 0
+    local c = 1
+    repeat
+        local width
+        init, str, width = head(str)
+        len = len + width
+        if len > num then
+            c = c + 1
+            len = width
+            res[c] = {}
+        end
+        table.insert(res[c], init)
+    until str == nil
+    for i = 1, #res do
+        res[i] = table.concat(res[i], "")
     end
-    table.insert(res[c], init)
-  until str == nil
-  for i = 1, #res do
-    res[i] = table.concat(res[i], "")
-  end
-  return res, (c == 1 and len or num)
+    return res, (c == 1 and len or num)
 end
 
 local current_win
 
 function M.close()
-  if current_win then
-    a.nvim_win_close(current_win.win, true)
-    a.nvim_buf_delete(current_win.buf, { force = true })
-    current_win = nil
-  end
+    if current_win then
+        a.nvim_win_close(current_win.win, true)
+        a.nvim_buf_delete(current_win.buf, { force = true })
+        current_win = nil
+    end
 end
 
 function M.create_window(lines, width)
-  local buf = a.nvim_create_buf(false, true)
-  a.nvim_buf_set_lines(buf, 0, -1, true, lines)
-  local win = a.nvim_open_win(buf, false, {
-    relative = "cursor",
-    width = width,
-    height = #lines,
-    style = "minimal",
-    row = 1,
-    col = 1,
-    border = "single",
-  })
-  current_win = { win = win, buf = buf }
+    local buf = a.nvim_create_buf(false, true)
+    a.nvim_buf_set_lines(buf, 0, -1, true, lines)
+    local win = a.nvim_open_win(buf, false, {
+        relative = "cursor",
+        width = width,
+        height = #lines,
+        style = "minimal",
+        row = 1,
+        col = 1,
+        border = "single",
+    })
+    current_win = { win = win, buf = buf }
 end
 
 local verb, dict
 
 function M.popup()
-  M.close()
-  local word = f.expand("<cword>")
-  if word == "" then
-    return
-  end
-  verb = verb or require("eiwa_dic.verb")
-  dict = dict or require("eiwa_dic.dict")
-  word = verb[word] or word
-  local meaning = dict[word]
-  if meaning then
-    local max = math.floor(a.nvim_win_get_width(0) * 0.6)
-    local lines, width = cut(meaning, max)
-    M.create_window(lines, width)
-  end
+    M.close()
+    local word = f.expand("<cword>")
+    if word == "" then
+        return
+    end
+    verb = verb or require("eiwa_dic.verb")
+    dict = dict or require("eiwa_dic.dict")
+    word = verb[word] or word
+    local meaning = dict[word]
+    if meaning then
+        local max = math.floor(a.nvim_win_get_width(0) * 0.6)
+        local lines, width = cut(meaning, max)
+        M.create_window(lines, width)
+    end
 end
 
 return M
--- misc/json2lua.lua
+++ misc/json2lua.lua
@@ -4,6 +4,6 @@ local name = "verb.json"
 print("return {")
 local json = require("json").decode(io.open(name):read("*a"))
 for k, v in pairs(json) do
-  print('["' .. k .. '"] = "' .. v:gsub('"', '\\"') .. '",')
+    print('["' .. k .. '"] = "' .. v:gsub('"', '\\"') .. '",')
 end
 print("}")
